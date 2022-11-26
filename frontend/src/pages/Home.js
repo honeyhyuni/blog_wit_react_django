768f729942e4bfd3f82733f471199e3a8e92ce54
honeyhyuni <tjfkqwkd001@naver.com>
2022-11-26 18:48:44 +0900

    URLMapping

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,8 @@
 import React from "react";
 import AppLayout from "components/AppLayout";
-import Example from "Example";
-
 function Home(){
     return(
         <AppLayout>
-            <Example />
         </AppLayout>
     );
 }
--- frontend/src/pages/accounts/index.js
+++ frontend/src/pages/accounts/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
 import Login from "./Login";
+import Logout from "./Logout";
 import Signup from "./Signup";
 // import LoginRequiredPage from "pages/utils/LoginRequiredPages";
 
@@ -10,6 +11,7 @@ function AccountsRoutes(){
         <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
+            <Route path="/logout" element={<Logout />} />
         </Routes>
     </>
     );
