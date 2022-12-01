3494597c516df38c06262055f420d783538a3cd0
honeyhyuni <tjfkqwkd001@naver.com>
2022-12-01 16:41:41 +0900

    DetailForms

---
+++ frontend/src/components/DetailForm/InformDetail.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import Axios from 'axios';
+import {Card} from  "antd";
+import DetailForm from 'components/DetailForm';
+import { useAxios } from 'use-axios';
+function InformDetail() {
+  const {user_id}  = useParams("");
+  const [post, setPost] = useState([]);
+  useEffect(() => {
+    async function fetchList(){
+    const apiUrl = `http://localhost:8000/api/inform/${user_id}`
+    await Axios.get(apiUrl)
+    .then(response => {
+        const {data} = response;
+        setPost(data)
+        console.log(data.data)
+    })
+    .catch()
+    }
+    fetchList()
+  },[])
+
+
+  return (
+    <div>
+       <DetailForm post={post}/>
+    </div>
+   )
+}
+
+export default InformDetail;
